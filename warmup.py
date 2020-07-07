5e8d1976050aa8f9beee47a68832b9804f0fd7b3
henryqin <henryqin@umich.edu>
2020-07-07 15:50:02 -0700

    novograd

--- warmup.py
+++ warmup.py
@@ -3,7 +3,7 @@ from helper import *
 from lars import LARS
 from lamb import Lamb
 from radam import RAdam
-import matplotlib.pyplot as plt
+from novograd import NovoGrad
 import sys
 import json
 
@@ -27,11 +27,37 @@ elif sys.argv[1] == 'lars':#no tensorboardX
     optimizer = LARS(model.parameters(), lr=0.1, momentum=0.9)
 elif sys.argv[1] == 'lamb':
     optimizer  = Lamb(model.parameters())
+elif sys.argv[1] == 'novograd':
+    optimizer = NovoGrad(model.parameters(), lr=0.01, weight_decay=0.001)
+    schedular = optim.lr_scheduler.CosineAnnealingLR(optimizer, 3 * len(train_loader), 1e-4)
 
-# elif sys.argv[1] == 'novograd':
-#     optimizer = NovoGrad(model.parameters(), lr=0.01, weight_decay=0.001)
-#     schedular = optim.lr_scheduler.CosineAnnealingLR(optimizer, 3 * len(train_loader), 1e-4)
-#     exit(0)
+
+    def train(train_loader, model, criterion, optimizer, schedular, device):
+        '''
+        Function for the training step of the training loop
+        '''
+
+        model.train()
+        running_loss = 0
+
+        for X, y_true in train_loader:
+            optimizer.zero_grad()
+
+            X = X.to(device)
+            y_true = y_true.to(device)
+
+            # Forward pass
+            y_hat, _ = model(X)
+            loss = criterion(y_hat, y_true)
+            running_loss += loss.item() * X.size(0)
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+            schedular.step()
+
+        epoch_loss = running_loss / len(train_loader.dataset)
+        return model, optimizer, epoch_loss
 
 else:
     optimizer = optim.SGD(model.parameters(), lr=0.01)
