407552eee5c2c3adc1ee6fc4236060a757bc407f
henryqin <henryqin@umich.edu>
2020-07-07 16:25:21 -0700

    update

---
+++ plot.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import json
+
+name_list = ['sgd','sgdwm','rmsprop','adagrad','adam','radam','lars','lamb','novograd']
+
+for name in name_list:
+    file = open('loss/'+name+'_loss.txt','r')
+    train_loss,valid_loss = json.load(file)
+    plt.plot(train_loss,label = name+'_train')
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed')
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
