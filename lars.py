c236ec21c1bf338f3a36f4ee95f14d2f637ee402
henryqin <henryqin@umich.edu>
2020-07-25 12:56:58 -0700

    update

--- lars.py
+++ lars.py
@@ -1,92 +1,96 @@
+""" Layer-wise adaptive rate scaling for SGD in PyTorch! """
 import torch
 from torch.optim.optimizer import Optimizer, required
 
 
 class LARS(Optimizer):
-    r"""Implements LARS (Layer-wise Adaptive Rate Scaling).
-
+    r"""Implements layer-wise adaptive rate scaling for SGD.
     Args:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
-        lr (float): learning rate
-        momentum (float, optional): momentum factor (default: 0)
-        eta (float, optional): LARS coefficient as used in the paper (default: 1e-3)
+        lr (float): base learning rate (\gamma_0)
+        momentum (float, optional): momentum factor (default: 0) ("m")
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        dampening (float, optional): dampening for momentum (default: 0)
-        nesterov (bool, optional): enables Nesterov momentum (default: False)
-        epsilon (float, optional): epsilon to prevent zero division (default: 0)
-
+            ("\beta")
+        eta (float, optional): LARS coefficient
+        max_epoch: maximum training epoch to determine polynomial LR decay.
+    Based on Algorithm 1 of the following paper by You, Gitman, and Ginsburg.
+    Large Batch Training of Convolutional Networks:
+        https://arxiv.org/abs/1708.03888
     Example:
-        >>> optimizer = torch.optim.LARS(model.parameters(), lr=0.1, momentum=0.9)
+        >>> optimizer = LARS(model.parameters(), lr=0.1, eta=1e-3)
         >>> optimizer.zero_grad()
         >>> loss_fn(model(input), target).backward()
         >>> optimizer.step()
     """
-
-    def __init__(self, params, lr=required, momentum=0, eta=1e-3, dampening=0,
-                 weight_decay=0, nesterov=False, epsilon=0):
+    def __init__(self, params, lr=required, momentum=.9,
+                 weight_decay=.0005, eta=0.001, max_epoch=200):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if momentum < 0.0:
             raise ValueError("Invalid momentum value: {}".format(momentum))
         if weight_decay < 0.0:
-            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+            raise ValueError("Invalid weight_decay value: {}"
+                             .format(weight_decay))
+        if eta < 0.0:
+            raise ValueError("Invalid LARS coefficient value: {}".format(eta))
 
-        defaults = dict(lr=lr, momentum=momentum, eta=eta, dampening=dampening,
-                        weight_decay=weight_decay, nesterov=nesterov, epsilon=epsilon)
-        if nesterov and (momentum <= 0 or dampening != 0):
-            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        self.epoch = 0
+        defaults = dict(lr=lr, momentum=momentum,
+                        weight_decay=weight_decay,
+                        eta=eta, max_epoch=max_epoch)
         super(LARS, self).__init__(params, defaults)
 
-    def __setstate__(self, state):
-        super(LARS, self).__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('nesterov', False)
-
-    def step(self, closure=None):
+    def step(self, epoch=None, closure=None):
         """Performs a single optimization step.
-
         Arguments:
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
+            epoch: current epoch to calculate polynomial LR decay schedule.
+                   if None, uses self.epoch and increments it.
         """
         loss = None
         if closure is not None:
             loss = closure()
 
+        if epoch is None:
+            epoch = self.epoch
+            self.epoch += 1
+
         for group in self.param_groups:
             weight_decay = group['weight_decay']
             momentum = group['momentum']
             eta = group['eta']
-            dampening = group['dampening']
-            nesterov = group['nesterov']
-            epsilon = group['epsilon']
+            lr = group['lr']
+            max_epoch = group['max_epoch']
 
             for p in group['params']:
                 if p.grad is None:
                     continue
-                w_norm = torch.norm(p.data)
-                g_norm = torch.norm(p.grad.data)
-                if w_norm * g_norm > 0:
-                    local_lr = eta * w_norm / (g_norm +
-                        weight_decay * w_norm + epsilon)
-                else:
-                    local_lr = 1
-                d_p = p.grad.data
-                if weight_decay != 0:
-                    d_p.add_(weight_decay, p.data)
-                if momentum != 0:
-                    param_state = self.state[p]
-                    if 'momentum_buffer' not in param_state:
-                        buf = param_state['momentum_buffer'] = torch.clone(d_p).detach()
-                    else:
-                        buf = param_state['momentum_buffer']
-                    buf.mul_(momentum).add_(1 - dampening, d_p)
-                    if nesterov:
-                        d_p = d_p.add(momentum, buf)
-                    else:
-                        d_p = buf
 
-                p.data.add_(-local_lr * group['lr'], d_p)
+                param_state = self.state[p]
+                d_p = p.grad.data
+
+                weight_norm = torch.norm(p.data)
+                grad_norm = torch.norm(d_p)
+
+                # Global LR computed on polynomial decay schedule
+                decay = (1 - float(epoch) / max_epoch) ** 2
+                global_lr = lr * decay
+
+                # Compute local learning rate for this layer
+                local_lr = eta * weight_norm / \
+                    (grad_norm + weight_decay * weight_norm)
+
+                # Update the momentum term
+                actual_lr = local_lr * global_lr
+
+                if 'momentum_buffer' not in param_state:
+                    buf = param_state['momentum_buffer'] = \
+                            torch.zeros_like(p.data)
+                else:
+                    buf = param_state['momentum_buffer']
+                buf.mul_(momentum).add_(actual_lr, d_p + weight_decay * p.data)
+                p.data.add_(-buf)
 
         return loss
--- plot.py
+++ plot.py
@@ -1,75 +1,136 @@
 from matplotlib import pyplot as plt
-import re
 import json
+import glob
+# name_list = ['sgd','sgdwm','rmsprop','adagrad','adam','radam','lars','lamb','novograd']
 
-name_list = ['sgd','sgdwm','rmsprop','adagrad','adam','radam','lars','lamb','novograd']
+#lb mnist lenet
+# name_list = ['sgd1.0','sgdwm0.1','rmsprop0.001','adagrad0.01','adam0.01','radam0.1','lars3.0','lamb0.01','novograd0.01']
+
+#sb cifar10 resnet56
+name_list = ['*lr0.1_sgd*','*lr0.1_sgdwm*','*lr0.0001_rmsprop*','*lr0.05_adagrad*','*lr0.001_adam*','*lr0.01_radam*','*lr1.0_lars*','*lr0.01_lamb*','*lr0.01_novograd*']
 
 color = ['r','g','b','c','y','k','m','violet','orange']
 
 markers = ['.','^','2','s','p','*','+','x','D']
 
-for i,name in enumerate(name_list):
-    file = open('loss/'+name+'_loss.txt','r')
-    train_loss,valid_loss = json.load(file)
-    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
-    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
-
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-
-for i,name in enumerate(['novograd','sgd','sgdwm']):
-    file = open('loss/'+name+'_loss.txt','r')
-    train_loss,valid_loss = json.load(file)
-    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
-    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
-
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-
-for i,name in enumerate(['novograd','adagrad','rmsprop']):
-    file = open('loss/'+name+'_loss.txt','r')
-    train_loss,valid_loss = json.load(file)
-    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
-    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
-
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-
-for i,name in enumerate(['novograd','adam','radam']):
-    file = open('loss/'+name+'_loss.txt','r')
-    train_loss,valid_loss = json.load(file)
-    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
-    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
-
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-
-for i,name in enumerate(['novograd','lars','lamb']):
-    file = open('loss/'+name+'_loss.txt','r')
-    train_loss,valid_loss = json.load(file)
-    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
-    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
-
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-# for name in name_list:
-#     file = open('log/'+name+'log.txt','r')
-#     valid_acc = []
-#     for line in file:
-#         valid_acc.append(float(line[-6:]))
-#     plt.plot(valid_acc,label = name)
+dir = 'logs/'
+# for i,name in enumerate(name_list):
+#     file = open('lbloss/'+name+'_loss.txt','r')
+#     train_loss,valid_loss,train_accuracy,valid_accuracy = json.load(file)
+#     plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+#     plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
 #
 # plt.xlabel('epoch')
-# plt.ylabel('valid accuracy')
+# plt.ylabel('loss')
 # plt.legend()
 # plt.show()
+
+
+#sb mnist-lenet
+#
+# for i,name in enumerate([name_list[8],name_list[0],name_list[1]]):
+#     file = open(dir+name+'_loss.txt','r')
+#     train_loss,valid_loss,train_accuracy,valid_accuracy = json.load(file)
+#     plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+#     plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+#
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
+#
+# for i,name in enumerate([name_list[8],name_list[3],name_list[2]]):
+#     file = open(dir+name+'_loss.txt','r')
+#     train_loss, valid_loss, train_accuracy, valid_accuracy = json.load(file)
+#     plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+#     plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+#
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
+#
+# for i,name in enumerate([name_list[8],name_list[4],name_list[5]]):
+#     file = open(dir+name+'_loss.txt','r')
+#     train_loss, valid_loss, train_accuracy, valid_accuracy = json.load(file)
+#     plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+#     plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+#
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
+#
+# for i,name in enumerate([name_list[8],name_list[6],name_list[7]]):
+#     file = open(dir+name+'_loss.txt','r')
+#     train_loss, valid_loss, train_accuracy, valid_accuracy = json.load(file)
+#     plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+#     plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+#
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
+
+
+#sb cifar10 resnet56
+for i,name in enumerate(name_list):
+    print(dir+name)
+    fn = glob.glob(dir+name)[0]
+    file = open(fn,'r')
+    train_loss,train_accuracy,valid_loss,valid_accuracy = json.load(file)
+    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+for i,name in enumerate([name_list[8],name_list[0],name_list[1]]):
+    fn = glob.glob(dir + name)[0]
+    file = open(fn, 'r')
+    train_loss, train_accuracy, valid_loss, valid_accuracy = json.load(file)
+    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+for i,name in enumerate([name_list[8],name_list[3],name_list[2]]):
+    fn = glob.glob(dir + name)[0]
+    file = open(fn, 'r')
+    train_loss, train_accuracy, valid_loss, valid_accuracy = json.load(file)
+    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+for i,name in enumerate([name_list[8],name_list[4],name_list[5]]):
+    fn = glob.glob(dir + name)[0]
+    file = open(fn, 'r')
+    train_loss, train_accuracy, valid_loss, valid_accuracy = json.load(file)
+    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+for i,name in enumerate([name_list[8],name_list[6],name_list[7]]):
+    fn = glob.glob(dir + name)[0]
+    file = open(fn, 'r')
+    train_loss, train_accuracy, valid_loss, valid_accuracy = json.load(file)
+    plt.plot(train_loss,label = name+'_train',color = color[i],marker = markers[i])
+    plt.plot(valid_loss,label = name+'_valid',linestyle='dashed',color = color[i],marker = markers[i])
+
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
