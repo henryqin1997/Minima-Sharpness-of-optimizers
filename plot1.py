66cf71639ca7c52d3a31fea7940daf87bf17d436
henryqin <henryqin@umich.edu>
2020-08-09 18:50:16 -0700

    scope issue fix

--- plot1.py
+++ plot1.py
@@ -1,6 +1,12 @@
 from matplotlib import pyplot as plt
 import json
-train_acc,valid_acc = json.load(open('original_loadbest_multistep_log.json','r'))
-print(valid_acc.index(max(valid_acc[:150])))
-print((valid_acc.index(max(valid_acc[150:250]))))
-print((valid_acc.index(max(valid_acc[250:]))))
+# train_acc,valid_acc = json.load(open('original_loadbest_multistep_log.json','r'))
+# print(valid_acc.index(max(valid_acc[:150])))
+# print((valid_acc.index(max(valid_acc[150:250]))))
+# print((valid_acc.index(max(valid_acc[250:]))))
+
+y = [(n+1)/(n+2) for n in range(100)]
+x = [1.1**i for i in range(-50,50)]
+plt.plot(x,y)
+plt.xscale('log')
+plt.show()
