f8b6a0d598ab3a39589887bc5a033b8bf1e404f9
henryqin <henryqin@umich.edu>
2020-08-16 12:47:50 -0700

    onecycle lr

---
+++ onecycle_make_ckpt.py
@@ -0,0 +1,217 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+import datetime
+
+from models import *
+from utils import progress_bar
+import json
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for testing (default: 128)')
+parser.add_argument('--warmup-epochs', type=int, default=5, metavar='WE',
+                    help='number of warmup epochs (default: 5)')
+parser.add_argument('--lr-decay', nargs='+', type=int, default=[50, 75],
+                    help='epoch intervals to decay lr')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--weight-decay', type=float, default=5e-4, metavar='W',
+                    help='SGD weight decay (default: 5e-4)')
+parser.add_argument('--optimizer',type=str,default='sgd',
+                    help='different optimizers')
+parser.add_argument('--max-lr',default=0.1,type=float)
+parser.add_argument('--div-factor',default=25,type=float)
+parser.add_argument('--final-div',default=10000,type=float)
+
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+ckptbest = './checkpoint/'+args.optimizer+str(args.lr)+'_ckptbest.pth'
+ckptinit = './checkpoint/'+args.optimizer+str(args.lr)+'_ckptinit.pth'
+ckptworst = './checkpoint/'+args.optimizer+str(args.lr)+'_ckptworst.pth'
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='/tmp/cifar10', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(
+    root='/tmp/cifar10', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+# net = VGG('VGG19')
+# net = ResNet18()
+# net = PreActResNet18()
+# net = GoogLeNet()
+# net = DenseNet121()
+# net = ResNeXt29_2x64d()
+# net = MobileNet()
+# net = MobileNetV2()
+# net = DPN92()
+# net = ShuffleNetG2()
+# net = SENet18()
+# net = ShuffleNetV2(1)
+# net = EfficientNetB0()
+# net = RegNetX_200MF()
+net = ResNet50()
+net = net.to(device)
+print('Saving init..')
+state = {
+    'net': net.state_dict(),
+    'acc': None,
+    'epoch': 0,
+}
+if not os.path.isdir('checkpoint'):
+    os.mkdir('checkpoint')
+torch.save(state, ckptinit)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+if args.optimizer.lower()=='sgd':
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+if args.optimizer.lower()=='sgdwm':
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
+                      weight_decay=args.weight_decay)
+elif args.optimizer.lower()=='adam':
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr,
+                      weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'rmsprop':
+    optimizer = optim.RMSprop(net.parameters(),lr=args.lr, momentum=args.momentum,
+                      weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'adagrad':
+    optimizer = optim.Adagrad(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'radam':
+    from radam import RAdam
+    optimizer = RAdam(net.parameters(),lr=args.lr,weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'lars':#no tensorboardX
+    from lars import LARS
+    optimizer = LARS(net.parameters(), lr=args.lr,momentum=args.momentum,weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'lamb':
+    from lamb import Lamb
+    optimizer  = Lamb(net.parameters(),lr=args.lr,weight_decay=args.weight_decay)
+elif args.optimizer.lower() == 'novograd':
+    from novograd import NovoGrad
+    optimizer = NovoGrad(net.parameters(), lr=args.lr,weight_decay=args.weight_decay)
+else:
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
+                          weight_decay=args.weight_decay)
+# lrs = create_lr_scheduler(args.warmup_epochs, args.lr_decay)
+# lr_scheduler = LambdaLR(optimizer,lrs)
+# lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, args.lr_decay, gamma=0.1)
+lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,args.max_lr,steps_per_epoch=len(trainloader),
+                                                   epochs=150,div_factor=args.div_factor,final_div_factor=args.final_div)
+train_acc = []
+valid_acc = []
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    train_acc.append(correct/total)
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100. * correct / total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, ckptbest)
+        best_acc = acc
+    valid_acc.append(correct/total)
+    if train_acc[-1]-valid_acc[-1]>0.5:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, ckptworst)
+
+for epoch in range(150):
+    train(epoch)
+    test(epoch)
+file = open(args.optimizer+str(args.max_lr/args.div_factor)+'-'+str(args.max_lr)+'-'+datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+'_onecycle_log.json','w+')
+json.dump([train_acc,valid_acc],file)
