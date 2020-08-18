8e7a5514d90cd707aed04f3ebd1272872376d669
henryqin <henryqin@umich.edu>
2020-08-18 01:15:44 -0700

    update

--- sharpness_measure_cifar10_resent.py
+++ sharpness_measure_cifar10_resent.py
@@ -1,7 +1,7 @@
 import torch
 
-ckpt = './checkpoint/novograd0.05_ckptbest.pth'
-
+#ckpt = './checkpoint/novograd0.05_ckptbest.pth'
+ckpt = './checkpoint/ckpt1.pth'
 print(torch.__version__)
 
 checkpoint = torch.load(ckpt)
