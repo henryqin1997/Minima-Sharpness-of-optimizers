ba5335a1fa12b1ff40e915fd1fc26ca7cb46cefc
henryqin <henryqin@umich.edu>
2020-08-19 19:45:57 -0700

    test

--- sharpness.py
+++ sharpness.py
@@ -7,23 +7,23 @@ def cal_sharpness(model,data_loader,criterion,epsilon_list=[1e-5]):
     #consider add one more, with eps(1+max(x)) as denominator
     res,res_eps,res_original = [],[],[]
     deep_copy_params = copy.deepcopy(model.state_dict())
+    currentloss = 0
+    print('calculating original loss')
+    model.eval()
+    total = 0
+    correct = 0
+    for batch_idx, (inputs, targets) in enumerate(data_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+        loss = criterion(outputs, targets)
+        currentloss += loss.item()
+    currentloss /= len(data_loader)
+    print(currentloss)
+    print('acc:' + str(correct / total))
     for eps in epsilon_list:
-        currentloss = 0
-        print('calculating original loss')
-        model.eval()
-        total = 0
-        correct = 0
-        for batch_idx, (inputs, targets) in enumerate(data_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-            loss = criterion(outputs, targets)
-            currentloss += loss.item()
-        currentloss /= len(data_loader)
-        print(currentloss)
-        print('acc:' + str(correct / total))
         optimizer = reverse_sgd.REV_SGD(model.parameters(),epsilon=eps)
         sharploss = 0
         print('calculating derivatives')
