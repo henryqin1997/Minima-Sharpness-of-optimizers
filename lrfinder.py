d8200c3e54dac7b1ec3574e726f84fef470afc45
henryqin <henryqin@umich.edu>
2020-08-20 00:16:19 -0700

    lr finder with gradient accumulate enable larger batchsize

--- lrfinder.py
+++ lrfinder.py
@@ -54,7 +54,7 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='/tmp/cifar10', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=args.batch_size, shuffle=True)
+    trainset, batch_size=128, shuffle=True)
 
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
