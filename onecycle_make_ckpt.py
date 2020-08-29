2985ce4f594f58eedf9833c5110bda7d50650468
henryqin <henryqin@umich.edu>
2020-08-28 19:04:36 -0700

    ckpt update

--- onecycle_make_ckpt.py
+++ onecycle_make_ckpt.py
@@ -37,6 +37,8 @@ parser.add_argument('--optimizer',type=str,default='sgd',
 parser.add_argument('--max-lr',default=0.1,type=float)
 parser.add_argument('--div-factor',default=25,type=float)
 parser.add_argument('--final-div',default=10000,type=float)
+parser.add_argument('--num-epoch',default=150,type=int)
+parser.add_argument('--pct-start',default=0.3,type=float)
 
 args = parser.parse_args()
 
@@ -127,7 +129,7 @@ else:
 # lr_scheduler = LambdaLR(optimizer,lrs)
 # lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, args.lr_decay, gamma=0.1)
 lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,args.max_lr,steps_per_epoch=len(trainloader),
-                                                   epochs=150,div_factor=args.div_factor,final_div_factor=args.final_div)
+                                                   epochs=args.num_epoch,div_factor=args.div_factor,final_div_factor=args.final_div,pct_start = args.pct_start)
 train_acc = []
 valid_acc = []
 
@@ -200,8 +202,10 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, ckptworst)
 
-for epoch in range(150):
+for epoch in range(args.num_epoch):
     train(epoch)
     test(epoch)
-file = open(args.optimizer+str(args.max_lr/args.div_factor)+'-'+str(args.max_lr)+'-'+datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+'_onecycle_log.json','w+')
+fn = '{}{}-{}-epoch{}-batchsize{}-pct{}-{}_onecycle_log.json'.format(args.optimizer,str(args.max_lr/args.div_factor),
+                            str(args.max_lr),args.num_epoch,args.batch_size,args.pct_start,datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+file = open(fn,'w+')
 json.dump([train_acc,valid_acc],file)
